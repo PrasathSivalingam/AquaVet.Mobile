dab1babd3833743a69d1ef99d6fdc97d77358e27
prasath <sprasath@gmail.com>
2014-04-22 23:19:19 +0200

    deviceready added

--- AquaVet.Mobile/App/main.js
+++ AquaVet.Mobile/App/main.js
@@ -5,8 +5,15 @@
         'durandal': '../scripts/durandal',
         'plugins': '../scripts/durandal/plugins',
         'transitions': '../scripts/durandal/transitions',
+        'bootstrap': '../scripts/bootstrap',
         'knockout': '../scripts/knockout-3.1.0',
         'jquery': '../scripts/jquery-2.1.0',
+    },
+    shim: {
+        'bootstrap': {
+            deps: ['jquery'],
+            exports: 'jQuery'
+        }
     }
 });
 
@@ -21,13 +28,46 @@ define(['durandal/app', 'durandal/viewLocator', 'durandal/system', 'plugins/rout
         system.debug(true);
 
         app.title = 'AquaVet';
-        
         // Specify which plugins to install und their configuation
         app.configurePlugins({
             router: true,
             dialog: true,
-            widget:true
+            widget: true
         });
+
+        var phonegapApp = {
+            // Application Constructor
+            initialize: function () {
+                this.bindEvents();
+            },
+            // Bind Event Listeners
+            //
+            // Bind any events that are required on startup. Common events are:
+            // 'load', 'deviceready', 'offline', and 'online'.
+            bindEvents: function () {
+                document.addEventListener('deviceready', this.onDeviceReady, false);
+            },
+            // deviceready Event Handler
+            //
+            // The scope of 'this' is the event. In order to call the 'receivedEvent'
+            // function, we must explicity call 'app.receivedEvent(...);'
+            onDeviceReady: function () {
+                phonegapApp.receivedEvent('deviceready');
+            },
+            // Update DOM on a Received Event
+            receivedEvent: function (id) {
+                var parentElement = document.getElementById(id);
+                var listeningElement = parentElement.querySelector('.listening');
+                var receivedElement = parentElement.querySelector('.received');
+
+                listeningElement.setAttribute('style', 'display:none;');
+                receivedElement.setAttribute('style', 'display:block;');
+
+                console.log('Received Event: ' + id);
+            }
+        };
+        
+
 
         app.start().then(function() {
 
@@ -56,5 +96,6 @@ define(['durandal/app', 'durandal/viewLocator', 'durandal/system', 'plugins/rout
 
             //Show the app by setting the root view model for our application.
             app.setRoot('viewmodels/shell', 'entrance');
+            phonegapApp.initialize();
         });
     });
